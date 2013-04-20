3b32f04db5b264b7a7399e9373d05cceeaa846ad
Niclas Magnusson <niclas@spelledare.se>
2013-04-20 19:04:34 +0200

    Secondary initial commit

--- config-sample.php
+++ config-sample.php
@@ -3,4 +3,8 @@ $dbhost = "<enter your database host, normally localhost>";
 $dbname = "<enter your database name>";
 $dbuser = "<enter your database username>";
 $dbpwd  = "<enter your database users password>";
+
+$title  = "<enter the title of your web site>";
+$slogan = "<enter the slogan of your web site>";
+
 ?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	require "config.php";
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?=$title?></title> 
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="utf-8">
+	</head> 
+ 	<body> 
+ 	<h1><?=$title?></h1>
+ 	<i><?=$slogan?></i>
+    Texten på sidan
+ 	</body>
+</html>
