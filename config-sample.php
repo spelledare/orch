098b52057ce91b54e144c7731255979d6566f601
Niclas Magnusson <niclas@aspire.(none)>
2013-04-20 18:50:06 +0200

    Initial commit

---
+++ config-sample.php
@@ -0,0 +1,6 @@
+<?php
+$dbhost = "<enter your database host, normally localhost>";
+$dbname = "<enter your database name>";
+$dbuser = "<enter your database username>";
+$dbpwd  = "<enter your database users password>";
+?>
